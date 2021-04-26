f1dbbe02d57e408682d289f2d5ec537b2493220a
LHWOOD <hutsonwood@gmail.com>
2021-04-26 09:09:38 -0600

    added header and styled

---
+++ Assets/js/script.js
@@ -0,0 +1,27 @@
+var questions = [
+  {
+    question: "Which of the following is not a commonly used data type?",
+    choices: ["numbers", "strings", "booradley", "arrays"],
+    answer: "booradley",
+  },
+  {
+    question: "Which of the following is a modulus arithmetic operator?",
+    choices: ["+", "-", "*", "%"],
+    answer: "%",
+  },
+  {
+    question: "The inputs of a function are called?",
+    choices: ["returns", "expressions", "parameters", "gits"],
+    answer: "",
+  },
+  {
+    question: "Which comparison operator means not equal to?",
+    choices: ["!==", "===", "<=", "/--"],
+    answer: "!==",
+  },
+  {
+    question: "Which of the following is a type of an iteration statement?",
+    choices: ["flexbox", "for loop", "console", "event"],
+    answer: "for loop",
+  },
+];
